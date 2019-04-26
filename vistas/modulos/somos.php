56be8b808430d8bf1dedb57e5254e94cdd3c1da7
RobertoVz <roberto@opalo.studio>
2019-04-26 17:49:19 -0500

    cambios terminados de la clase del viernes 26 de abril

--- vistas/modulos/somos.php
+++ vistas/modulos/somos.php
@@ -1,3 +1,21 @@
 <section class="somos">
-  <h1>Hola2</h1>
+<div class="row">
+  <div class="col-6 zonaImg">
+    <div class="imgs">
+      <img class="imgPerfil" src="vistas/img/foto.svg">
+      <img class="imgPerfil" src="vistas/img/foto.svg">
+      <img class="imgPerfil" src="vistas/img/foto.svg">
+      <img class="imgPerfil" src="vistas/img/foto.svg">
+      <img class="imgPerfil" src="vistas/img/foto.svg">
+      <img class="imgPerfil" src="vistas/img/foto.svg">
+    </div>
+    
+  </div>
+  
+  <div class="col-6 zonaTexto">
+    <div class="texto">
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui eaque sapiente quis cumque asperiores reprehenderit dolorem, eveniet pariatur repellendus laboriosam earum distinctio ipsa magnam excepturi dolorum error consequatur? Error, possimus?</p>
+    </div>
+  </div>
+</div>
 </section>
