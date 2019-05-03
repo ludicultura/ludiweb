cf02f898fcd584d73457a606856999529131aabb
RobertoVz <roberto@opalo.studio>
2019-05-03 16:39:31 -0500

    ultimos cambios realizados en galeria en la clase del 4 de mayo del 2019

--- vistas/modulos/galeria.php
+++ vistas/modulos/galeria.php
@@ -1,4 +1,41 @@
-<section class="home">
-  <h1>Hola</h1>
-  <a type="button" href="?page=1" class="btn btn-primary">Primary</a>
+<section class="galeria">
+<h1 class="titulo">Proyectos</h1>
+
+<div class="contenedorCards">
+  <div class="cardProyecto">
+  <img class="imgProyecto" src="vistas/img/foto.svg">
+  <div class="barritaCard"></div>
+  <p class="textoCard">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ullam nihil sed sint nam asperiores minus vitae quam doloribus illum voluptate officiis ipsum voluptates nulla ipsam, eligendi error, veniam sapiente.</p>
+  </div>
+  
+  <div class="cardProyecto">
+  <img class="imgProyecto" src="vistas/img/foto.svg">
+  <div class="barritaCard"></div>
+  <p class="textoCard">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ullam nihil sed sint nam asperiores minus vitae quam doloribus illum voluptate officiis ipsum voluptates nulla ipsam, eligendi error, veniam sapiente.</p>
+  </div>
+  
+  <div class="cardProyecto">
+  <img class="imgProyecto" src="vistas/img/foto.svg">
+  <div class="barritaCard"></div>
+  <p class="textoCard">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ullam nihil sed sint nam asperiores minus vitae quam doloribus illum voluptate officiis ipsum voluptates nulla ipsam, eligendi error, veniam sapiente.</p>
+  </div>
+  
+  <div class="cardProyecto">
+  <img class="imgProyecto" src="vistas/img/foto.svg">
+  <div class="barritaCard"></div>
+  <p class="textoCard">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ullam nihil sed sint nam asperiores minus vitae quam doloribus illum voluptate officiis ipsum voluptates nulla ipsam, eligendi error, veniam sapiente.</p>
+  </div>
+  
+  <div class="cardProyecto">
+  <img class="imgProyecto" src="vistas/img/foto.svg">
+  <div class="barritaCard"></div>
+  <p class="textoCard">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ullam nihil sed sint nam asperiores minus vitae quam doloribus illum voluptate officiis ipsum voluptates nulla ipsam, eligendi error, veniam sapiente.</p>
+  </div>
+  
+  <div class="cardProyecto">
+  <img class="imgProyecto" src="vistas/img/foto.svg">
+  <div class="barritaCard"></div>
+  <p class="textoCard">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ullam nihil sed sint nam asperiores minus vitae quam doloribus illum voluptate officiis ipsum voluptates nulla ipsam, eligendi error, veniam sapiente.</p>
+  </div>
+</div>
 </section>
