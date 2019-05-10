a4fe0730f6a86a53fb205f3ea32e9c1838f0a126
RobertoVz <roberto@opalo.studio>
2019-05-10 16:36:30 -0500

    implementados un boton qu ejecuta una funcion

---
+++ controladores/controlador.login.php
@@ -0,0 +1,14 @@
+<script>
+
+
+
+function esconder(){        //Se declara la funcion esconder
+    $('.imgs').fadeOut();   //Con Jquery se selecciona el div con clase  imgs y se realiza un fadeOut
+}
+
+function mostrar(){
+    $('.imgs').fadeIn();
+}
+
+
+</script>
--- vistas/modulos/somos.php
+++ vistas/modulos/somos.php
@@ -15,7 +15,12 @@
   <div class="col-6 zonaTexto">
     <div class="texto">
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui eaque sapiente quis cumque asperiores reprehenderit dolorem, eveniet pariatur repellendus laboriosam earum distinctio ipsa magnam excepturi dolorum error consequatur? Error, possimus?</p>
+      
     </div>
+
+    <!--Crean un boton y despues con el "onclick" mandar a ejecutar la funcion-->
+    <button class="btnEsconder" onclick="esconder()">Esconder monitos</button>
+    <button class="btnEsconder" onclick="mostrar()">Mostar monitos</button>
   </div>
 </div>
 </section>
