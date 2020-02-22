84c0a918aee083f8ce42d2c0f01914474c575c79
Oscar <isf6323@umad.edu.mx>
2020-02-21 18:43:30 -0600

    maquetado de:impacto,nosotros y participa

--- vistas/modulos/impacto.php
+++ vistas/modulos/impacto.php
@@ -1,3 +1,26 @@
 <section class="impacto d-flex flex-column justify-content-center align-items-center">
-    <img class="logoImpacto" src="vistas/img/impacto.png">
+    <h1 class="text-center">¡NUESTRO IMPACTO!</h1>
+    <img class="logoImpacto" src="vistas/img/imagen_impacto.svg">
+</section>
+
+<section class="impacto d-flex flex-column justify-content-center align-items-center">
+
+  <img class="logosImpacto" src="vistas/img/imagen01.svg">
+  <h1 class="text-center">JUEGOS IMPLEMENTADOS</h1>  
+  
+</section>
+
+<section class="impacto d-flex flex-column justify-content-center align-items-center">
+  <img class="logosImpacto" src="vistas/img/imagen03.svg">
+  <h1 class="text-center">BENEFICIARIOS</h1>  
+</section>
+
+<section class="impacto d-flex flex-column justify-content-center align-items-center">
+  <img class="logosImpacto" src="vistas/img/imagen05.svg">
+  <h1 class="text-center">ALIADOS</h1>    
+</section>
+
+<section class="impacto d-flex flex-column justify-content-center align-items-center">
+  <img class="logosImpacto" src="vistas/img/imagen07.svg">
+  <h1 class="text-center">EN EL FUTURO</h1>    
 </section>
---
+++ vistas/modulos/nosotros.php
@@ -0,0 +1,15 @@
+<section class="nosotros d-flex flex-column justify-content-center align-items-center">
+    <h1 class="text-center">NOSOTROS</h1>
+    <img class="logoNosotros" src="vistas/img/imagen_nosotros.svg">
+</section>
+
+<section class="nosotros  d-flex flex-column justify-content-center align-items-center">
+  <h1 class="text-center">NOSOTROS</h1>
+  <p>Somos expertos en el diseño de juegos, actividades de integración y acciones de responsabilidad social</p>
+  <p>"Transformamos a México con arte, cultura y diversión"</p>
+</section>
+
+<section class="nosotros  d-flex flex-column justify-content-center align-items-center">
+  <h1 class="text-center">NUESTROS ALIADOS</h1>
+  <img class="logosNosotros" src="vistas/img/imagen03.svg">
+</section>
