7749cfc62e048d87a681953f7d973cf1c154b357
Oscar <isf6323@umad.edu.mx>
2020-03-02 17:35:37 -0600

    footer de impacto,nosotros y participa

--- vistas/modulos/nosotros.php
+++ vistas/modulos/nosotros.php
@@ -1,6 +1,6 @@
 <section class="nosotros d-flex flex-column justify-content-center align-items-center">
     <h1 class="text-center">NOSOTROS</h1>
-    <img class="logoNosotros" src="vistas/img/imagen_nosotros.svg">
+    <img class="logoNosotros" src="">
 </section>
 
 <section class="nosotros  d-flex flex-column justify-content-center align-items-center">
@@ -11,5 +11,27 @@
 
 <section class="nosotros  d-flex flex-column justify-content-center align-items-center">
   <h1 class="text-center">NUESTROS ALIADOS</h1>
-  <img class="logosNosotros" src="vistas/img/imagen03.svg">
+  <img class="logosNosotros" src="">
+</section>
+
+<section class="navegacionInferior">
+    <div class="row">
+      <div class="col-sm-4 bordesCom">
+        <a class="link-barra-inferior" href="?page=3">
+          <p>
+          <h5 >Nosotros</h5>    
+        </a>
+      </div>
+      <div class="col-sm-4 bordesCom">
+        <a href="index.php">
+          <img class="logoMini" src="vistas/img/imagen_ludi_blanco.svg">
+        </a>
+      </div>
+      <div class="col-sm-4 bordesCom">
+        <a class="link-barra-inferior" href="?page=5">
+          <p>
+          <h5 >Participa</h5>    
+        </a>
+      </div>
+    </div>
 </section>
